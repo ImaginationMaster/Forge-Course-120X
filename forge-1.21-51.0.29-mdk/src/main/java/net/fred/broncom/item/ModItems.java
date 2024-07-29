344344d4ec9e162b9a098554573e88fc9bd9ce8f
ImaginationMaster <fredchild2016@outlook.com>
2024-07-29 17:18:35 +0100

    Adding an item WIP

---
+++ forge-1.21-51.0.29-mdk/src/main/java/net/fred/broncom/item/ModItems.java
@@ -0,0 +1,23 @@
+package net.fred.broncom.item;
+
+import net.fred.broncom.BroncomMod;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.rmi.registry.Registry;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, BroncomMod.MOD_ID);
+
+    public static final RegistryObject<Item> ENDERITE = ITEMS.register( "enderite",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
